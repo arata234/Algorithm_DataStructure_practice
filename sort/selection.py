c2310d37d8d6761855ea0a6fbb9171e99a9a5935
arata234 <90789032+arata234@users.noreply.github.com>
2023-03-18 19:45:02 +0900

    Create selection.py

---
+++ sort/selection.py
@@ -0,0 +1,33 @@
+import numpy as np
+import time
+
+
+def selection_sort(unsorted_array):
+    len_array = len(unsorted_array)
+    start = 0
+    end = len_array
+    for i in range(start, end - 1):
+        num = unsorted_array[i]
+        index = i
+        for j in range(i+1, end):
+            if num > unsorted_array[j]:
+                index = j
+                num = unsorted_array[j]
+        unsorted_array[i], unsorted_array[index] = unsorted_array[index], unsorted_array[i]
+    sorted_array = unsorted_array.copy()
+    return sorted_array
+
+def is_sorted(sorted_array):
+    for i in range(len(sorted_array)-1):
+        if sorted_array[i] > sorted_array[i+1]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    unsorted_array = np.random.randint(0, 10000, 100)
+    start = time.time()
+    array= selection_sort(unsorted_array)
+    print("time: ", time.time() - start)
+    print(array)
+    print(is_sorted(array))
