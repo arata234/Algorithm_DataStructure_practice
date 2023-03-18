52a22cfb70b3cec93a85c1658b78bd2ad30ae3e5
arata234 <90789032+arata234@users.noreply.github.com>
2023-03-18 19:03:59 +0900

    Create cocktail.py

---
+++ sort/cocktail.py
@@ -0,0 +1,39 @@
+import numpy as np
+import time
+
+
+def cocktail_sort(unsorted_array):
+    len_array = len(unsorted_array)
+    swapped = True
+    start = 0
+    end = len_array - 1
+    c = 0
+    while swapped:
+        swapped = False
+        for i in range(start, end):
+            if unsorted_array[i] > unsorted_array[i+1]:
+                swapped = True
+                unsorted_array[i], unsorted_array[i+1] = unsorted_array[i+1], unsorted_array[i]
+        c += 1
+        if not swapped:
+            break
+        
+        swapped = False
+        end -=1
+        for i in range(end-1, start-1, -1):
+            if unsorted_array[i] > unsorted_array[i+1]:
+                swapped = True
+                unsorted_array[i], unsorted_array[i+1] = unsorted_array[i+1], unsorted_array[i]
+        start += 1
+        c += 1
+        
+    sorted_array = unsorted_array.copy()
+    return sorted_array, c
+
+
+if __name__ == "__main__":
+    unsorted_array = np.random.randint(0, 100, 10000)
+    start = time.time()
+    array, c = cocktail_sort(unsorted_array)
+    print("time: ", time.time() - start)
+    print(array, c)
