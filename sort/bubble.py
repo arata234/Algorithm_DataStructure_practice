1a38e3063d99f61bd21c113a4ee1b1627a8e1e19
arata234 <90789032+arata234@users.noreply.github.com>
2023-03-18 16:39:39 +0900

    Create bubble.py

---
+++ sort/bubble.py
@@ -0,0 +1,20 @@
+import numpy as np
+import time
+
+
+
+def bubble_sort(unsorted_array):
+    len_array = len(unsorted_array)
+    for i in range(len_array):
+        for j in range(len_array-i-1):
+            if unsorted_array[j] > unsorted_array[j+1]:
+                unsorted_array[j], unsorted_array[j+1] = unsorted_array[j+1], unsorted_array[j]
+    sorted_array = unsorted_array.copy()
+    return sorted_array
+
+if __name__ == "__main__":
+    unsorted_array = np.random.randint(0, 100, 100)
+    start = time.time()
+    array= bubble_sort(unsorted_array)
+    print("time: ", time.time() - start)
+    print(array)
