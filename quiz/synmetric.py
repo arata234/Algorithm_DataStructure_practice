b62ca9c658ced2e698273ef72ae7e98afe0a1e80
arata234 <90789032+arata234@users.noreply.github.com>
2023-03-24 00:10:46 +0900

    Update synmetric.py

--- quiz/synmetric.py
+++ quiz/synmetric.py
@@ -8,17 +8,16 @@ def find_pair(pairs: List[Tuple[int, int]]) -> Iterator[tuple[int, int]]:
     for pair in pairs:
         first, second = pair[0], pair[1]
         cache[second].append(first)
-    
+
     for pair in pairs:
         first, second = pair[0], pair[1]
-        for value in cache[first]:
-            if value == second:
-                yield pair
+        if first in cache:
+            for value in cache[first]:
+                if value == second:
+                    print(pair)
         
 
 
-
-
 if __name__ == "__main__":
-    input = [(1, 2), (3, 5), (4, 7), (5, 3), (7, 3)]
+    input = [(1, 2), (3, 5), (4, 7), (5, 3), (7, 4)]
     find_pair(input)
