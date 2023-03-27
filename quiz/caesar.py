272e1cebf7535f2c69835a98a3846f83a5072b03
arata234 <90789032+arata234@users.noreply.github.com>
2023-03-27 14:37:05 +0900

    Update caesar.py

--- quiz/caesar.py
+++ quiz/caesar.py
@@ -1,5 +1,17 @@
 import string
 
 
-def caesar_cipher(text: strm shift: int) -> str:
+def caesar_cipher(text: str, shift: int) -> str:
+    result = ""
+    for char in text:
+        if char.isupper():
+            alphabet = string.ascii_uppercase
+        elif char.islower():
+            alphabet = string.ascii_lowercase
+        else:
+            result += char
+            continue
+    print(alphabet)
+
+if __name__ == "__main__":
     
